6d4f6e798779f71bbff551135cd6ca7c4c36826f
Genaro Canche Peña <genaro.canche@icorp.com.local>
2018-03-23 20:59:48 -0500

    Subiendo proyecto

---
+++ src/components/HomePage.vue
@@ -0,0 +1,69 @@
+<template>
+  <swiper :options="swiperOption">
+      <!-- Slides -->
+      <swiper-slide>
+        <Movies image="http://res.cloudinary.com/og-tech/image/upload/s--4NgMf3RF--/v1521804358/avengers.jpg" title="Avengers : Infinity War" description="Thanos is around" duration="2hrs"/>
+      </swiper-slide>
+
+      <swiper-slide>
+        <Movies image="http://res.cloudinary.com/og-tech/image/upload/s--BmgguRnX--/v1521804402/thor.jpg" title="Thor : Ragnarok" description="Thor lost his hair" duration="2hrs30mins"/>
+      </swiper-slide>
+
+      <swiper-slide>
+        <Movies image="http://res.cloudinary.com/og-tech/image/upload/s--qXaW5V3E--/v1521804426/wakanda.jpg" title="Black Panther" description="Wakanda Forever" duration="2hrs15mins"/>
+      </swiper-slide>
+        <!-- Control: opcional -->
+      <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
+</template>
+
+<style scoped>
+.swiper-slide {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+}
+.swiper-container {
+    height:500px;
+}
+</style>
+
+
+<script>
+import Movies from './Movies'
+import 'swiper/dist/css/swiper.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+export default {
+  name: 'HomePage',
+  components: {
+      Movies,
+      swiper,
+      swiperSlide
+  },
+  data() {
+      return {
+          swiperOption: {
+              /*direction: 'vertical',
+              pagination: {
+                  el: '.swiper-pagination',
+                  type: 'bullets'
+              },*/
+              effect: 'coverflow',
+              grabCursor: true,
+              centeredSlides: true,
+              slidesPerView: '5',
+              coverflowEffect: {
+                  rotate: 50,
+                  stretch: 0,
+                  depth: 100,
+                  modifier: 1,
+                  slideShadows: false,
+              },
+              pagination: {
+                  el: '.swiper-pagination'
+              }
+          }
+      }
+  }
+}
+</script>
---
+++ src/components/Movies.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="movie" v-bind:style="{ backgroundImage: 'url(' + image + ')' }">
+        <div class="content">
+            <h1 class="title">{{ title }}</h1>
+            <p class="description">{{ description }}</p>
+            <p class="duration">{{ duration }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+h1, h2 {
+    font-weight:bold;
+}
+.movie {
+    display:flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-end;
+    text-align: left;
+    padding: 10px;
+    width: 350px;
+    height:500px;
+    background-color: rgba(255, 255, 255, .4);
+    background-repeat: no-repeat;
+    background-blend-mode: overlay;
+    background-size: cover;
+}
+</style>
+
+
+<script>
+    export default {
+        name: 'Movies',
+        props: [
+            'image',
+            'title',
+            'description',
+            'duration'
+        ],
+    }
+</script>
